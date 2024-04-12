96da75d56bd6f82da85cd56eab9b4f8480ec4355
Yara-ib <yr.ap.23@gmail.com>
2024-04-13 00:34:38 +0200

    sQuaRe

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -6,7 +6,6 @@ class Square():
     """Square Class """
 
     width = 0
-    height = 0
 
     def __init__(self, *args, **kwargs):
         """ Initialization for instances """
@@ -15,20 +14,20 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.width * self.width
 
     def perimeter_of_my_square(self):
         """ Perimeter of the Square """
-        return (self.width * 2) + (self.height * 2)
+        return (self.width * 4)
 
     def __str__(self):
         """ Returning String format """
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.width, self.width)
 
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=12)
+    s = Square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
