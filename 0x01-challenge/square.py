08b387e6506b54d9ddba2ee682c7054ff0cbbf8b
Yara-ib <yr.ap.23@gmail.com>
2024-04-13 00:04:05 +0200

    Tasks

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,29 +2,27 @@
 """ Square Module """
 
 
-class square():
-    """Square Class """
-    width = 0
-
-    def __init__(self, *args, **kwargs):
-        """ Initialization for instances """
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
-
-    def PermiterOfMySquare(self):
-        """ Perimeter of the Square """
-        return (self.width * 4)
-
-    def __str__(self):
-        """ Returning String format """
-        return "{}/{}".format(self.width, self.width)
-
-
 if __name__ == "__main__":
+    class square():
+        """Square Class """
+        width = 0
+
+        def __init__(self, *args, **kwargs):
+            """ Initialization for instances """
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+        def area_of_my_square(self):
+            """ Area of the square """
+            return self.width * self.width
+
+        def PermiterOfMySquare(self):
+            """ Perimeter of the Square """
+            return (self.width * 4)
+
+        def __str__(self):
+            """ Returning String format """
+            return "{}/{}".format(self.width, self.width)
 
     s = square(width=12, height=9)
     print(s)
