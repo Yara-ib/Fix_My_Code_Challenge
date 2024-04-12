a451c9de4817e846c25ae30e45ddf2e22cafdca2
Yara-ib <yr.ap.23@gmail.com>
2024-04-13 00:08:06 +0200

    sQuaRe

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,29 +2,32 @@
 """ Square Module """
 
 
+class square():
+    """Square Class """
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """ Initialization for instances """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def PermiterOfMySquare(self):
+        """ Perimeter of the Square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ Returning String format """
+        return "{}/{}".format(self.width, self.height)
+
+
 if __name__ == "__main__":
-    class square():
-        """Square Class """
-        width = 0
 
-        def __init__(self, *args, **kwargs):
-            """ Initialization for instances """
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-        def area_of_my_square(self):
-            """ Area of the square """
-            return self.width * self.width
-
-        def PermiterOfMySquare(self):
-            """ Perimeter of the Square """
-            return (self.width * 4)
-
-        def __str__(self):
-            """ Returning String format """
-            return "{}/{}".format(self.width, self.width)
-
-    s = square(width=12, height=9)
+    s = square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
